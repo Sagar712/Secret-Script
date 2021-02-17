54974d6946ad7875c93754a76c63774555f1ad41
Sagar712 <74832706+Sagar712@users.noreply.github.com>
2021-02-17 15:18:59 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
 
-function scriptmath(variable1){
-    let a = 'a';
-    console.log(a.charCodeAt(0));
-    let b = String.fromCharCode(a.charCodeAt(0)+5);
-    console.log(b);
-    let c='986';
-    let no = parseInt(c.charAt(0));
-    console.log(no);
+let login = "false";
+
+function scriptmath(){
     if(document.getElementById("heading").style.color!="yellow")
         document.getElementById("heading").style.color="yellow";
     else
     document.getElementById("heading").style.color="white";
-}
 
-function readata(){
-    let key = document.getElementById("keyy").value;
-    let msg = document.getElementById("keyy2").value;
-    let flip = '';
-    //megging flipping mechanism
-    for(let i=msg.length-1; i>=0; i--) {
-        flip = flip.concat(msg.charAt(i)).toLowerCase();
-    }
-
-    document.getElementById("encryted").innerHTML=flip;
-    console.log("Encrypt clicked");
+    let temp = sessionStorage.getItem("loginstr");
+    console.log(temp);
 }
 
 function reset(){
@@ -37,32 +22,47 @@ function encrypt(){
     let msg = document.getElementById("keyy2").value;
     if(key!=""){
         let encypted="";
-		let j =0, looper=0, len = key.length;
+		let looper=0, len = key.length;
+        let conv;
         let exchange=0;
-        let series = [' ','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/',
-        '0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?','@',
-        'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-        '[','\\',']','^','_','`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u',
-        'v','w','x','y','z','{','|','}','~'];
+        let firstspace = true;
+        let series = [" ","!","'","#","$","%","&","\"","(",")","*","+",",","-",".","/",
+        "0","1","2","3","4","5","6","7","8","9",":",";","<","=",">","?","@",
+        "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+        "[","\\","]","^","_","`","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u",
+        "v","w","x","y","z","{","|","}","~"];
 
-		for(let i=0; i<msg.length; i++) {
-			j= parseInt(key.charAt(looper));
-			for(let k=0; k<series.length; k++) {
-				if(series[k] == msg.charAt(i)) {
-					exchange = k;
-					break;
-				}
-			}
-			let conv = series[((exchange+j)%95)];
-			
-			encypted = encypted.concat(conv);
-			looper = (looper+1)%len;
-		}
-
-		console.log("Encrypted: "+encypted);
-        document.getElementById("encryted").innerHTML=encypted;
-        document.getElementById("crypted").innerHTML="Encrypted Message:";
-    }
+        for(let i=0; i<msg.length; i++){
+            let j=parseInt(key.charAt(looper));
+            for(let k=0; k<series.length; k++){
+                if(series[k]===msg.charAt(i)){
+                    exchange=k;
+                    break;
+                }
+            }
+            conv = series[(exchange+j)%95];
+            if(firstspace==true){
+                if(conv==" ")
+                break;
+            }
+            firstspace=false;
+            if(i === msg.length-2)
+                firstspace=true;
+            encypted = encypted.concat(conv);
+            looper = (looper+1)%len;
+        }
+        if(firstspace==true){
+            document.getElementById("crypted").innerHTML=`Please change ${looper+1}'th digit to avoid SPACE error <br/>This occurs when encrypted code is having first or last letter as a SPACE`;
+            document.getElementById("encryted").innerHTML="! Warning !<br/>SPACE at the beginning or at the end";
+        }
+        
+        else{
+            console.log("Encrypted:"+encypted);
+            document.getElementById("encryted").innerText=encypted;
+            document.getElementById("crypted").innerHTML="Encrypted Message:";
+        }
+        
+    }	
     else
         alert("key not entered");
     
@@ -75,16 +75,16 @@ function decrypt(){
 		let encypted="";
 		let j =0, looper=0, len = key.length;
         let exchange=0;
-        let series = [' ','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/',
-        '0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?','@',
-        'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-        '[','\\',']','^','_','`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u',
-        'v','w','x','y','z','{','|','}','~'];
+        let series = [" ","!","'","#","$","%","&","\"","(",")","*","+",",","-",".","/",
+        "0","1","2","3","4","5","6","7","8","9",":",";","<","=",">","?","@",
+        "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+        "[","\\","]","^","_","`","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u",
+        "v","w","x","y","z","{","|","}","~"];
 
 		for(let i=0; i<msg.length; i++) {
 			j= parseInt(key.charAt(looper));
 			for(let k=0; k<series.length; k++) {
-				if(series[k] == msg.charAt(i)) {
+				if(series[k] === msg.charAt(i)) {
 					exchange = k;
 					break;
 				}
@@ -101,7 +101,7 @@ function decrypt(){
 		}
 
 		console.log("Decrypted: "+encypted);
-        document.getElementById("encryted").innerHTML=encypted;
+        document.getElementById("encryted").innerText=encypted;
         document.getElementById("crypted").innerHTML="Decrypted Message:";
     }
     else
@@ -110,10 +110,27 @@ function decrypt(){
 
 const menu = document.querySelector(".inck");
 const menuLinks = document.querySelector(".dropdown");
+const overlay = document.querySelector(".overlay");
 let a=0;
+
+function overlayhandle(){
+    menuLinks.classList.toggle("active");
+    menu.classList.toggle("active");
+    overlay.classList.toggle("active");
+    if(a%2!=0){
+        document.getElementById("changeicon").className="fas fa-bars";
+        a++;
+    }
+    else{
+        document.getElementById("changeicon").className="fas fa-times";
+        a++;
+    }
+}
 
 menu.addEventListener('click', function handler(){
     menuLinks.classList.toggle("active");
+    menu.classList.toggle("active");
+    overlay.classList.toggle("active");
     if(a%2!=0){
         document.getElementById("changeicon").className="fas fa-bars";
         a++;
@@ -123,3 +140,21 @@ menu.addEventListener('click', function handler(){
         a++;
     }
 });
+
+function logincontrol(){
+    let user = document.getElementById("user").value;
+    let pass = document.getElementById("pass").value;
+
+    if(user == "admin" && pass == "sagar123"){
+        window.location.href="./application.html";
+        login = "true";
+        sessionStorage.setItem("loginstr", login);
+    }
+    else{
+        document.getElementById("outpu").innerHTML="Invalid username or password";
+        login = "false";
+        sessionStorage.setItem("loginstr", login);
+    }
+        
+
+}
